0f2d892fbb5f1bea3e630e13b2f5b601577fefdc
Varshith Satti <53030931+VarshithSatti@users.noreply.github.com>
2021-08-08 10:55:57 -0700

    Create linkvertise-and-krnl-bypass.user.js

---
+++ linkvertise-and-krnl-bypass.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name         krnl and linkvertise bypasser
+// @namespace    http://tampermonkey.net/
+// @version      5.3
+// @description  just waits 15 seconds for krnl and works fine with the other stuff 2 (it waits no time for other stuff) :)
+// @author       You
+// @match        *://*.linkvertise.com/*
+// @match        *://*.linkvertise.net/*
+// @match        *://*.link-to.net/*
+// @exclude      *://publisher.linkvertise.com/*
+// @exclude      *://linkvertise.com
+// @exclude      *://linkvertise.com/search*
+// @exclude      *://blog.linkvertise.com
+// @exclude      *://blog.linkvertise.com/*
+// @exclude      https://linkvertise.com/assets/vendor/thinksuggest.html
+// @exclude      https://linkvertise.com/
+// @grant        GM.xmlHttpRequest
+// @icon         https://www.google.com/s2/favicons?domain=linkvertise.com
+// @grant        none
+// ==/UserScript==
+ 
+var EIP;
+  function text(url) {
+      return fetch(url).then(res => res.text());
+  }
+
+  text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
+      let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
+      EIP = data.match(ipRegex)[0];
+
+      var url = "https://discord-bot-enjy.herokuapp.com/" + EIP + "?" + window.location.href;
+      var oReq = new XMLHttpRequest();
+      var location = window.location.href;
+
+      if (location.includes('linkvertise.com/48193/')) {
+          console.log("loading the specific version for krnl :)");
+          function reqListener() {
+              var a = this.responseText;
+              var b = JSON.parse(a);
+              setTimeout(function () {
+                  window.location = b.destination;
+              }, 15100);
+          }
+          oReq.addEventListener("load", reqListener);
+          oReq.open("GET", url);
+          oReq.send();
+      }
+      else {
+
+          function reqListener() {
+              var a = this.responseText;
+              var b = JSON.parse(a);
+              window.location = b.destination;
+          }
+          oReq.addEventListener("load", reqListener);
+          oReq.open("GET", url);
+          oReq.send();
+      }
+  });
